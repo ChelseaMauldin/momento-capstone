832cb44e9a5bb28494c4f032c91f0647e95e73b2
Chelsea Mauldin <clmauldin5962@gmail.com>
2023-02-03 12:52:10 -0500

    progress on search bar function

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -1,20 +1,45 @@
 <template>
   <div id="app">
     <div id="nav">
-      <i class="fa-solid fa-house-user">&nbsp;<router-link class="link-text" v-bind:to="{ name: 'home' }">Home</router-link></i>
-      <i v-if="$store.state.token == ''" class="fa-solid fa-arrow-right-to-bracket">&nbsp;<router-link class="link-text" v-bind:to="{ name: 'login' }">Login</router-link></i>
-      <i v-if="$store.state.token != ''" class="fa-solid fa-arrow-right-to-bracket"><router-link class="link-text"
-        v-bind:to="{ name: 'logout' }"
-        >Logout</router-link
-      ></i>
+      <i class="fa-solid fa-house-user"
+        >&nbsp;<router-link class="link-text" v-bind:to="{ name: 'home' }"
+          >Home</router-link
+        ></i
+      >
+      <i
+        v-if="$store.state.token == ''"
+        class="fa-solid fa-arrow-right-to-bracket"
+        >&nbsp;<router-link class="link-text" v-bind:to="{ name: 'login' }"
+          >Login</router-link
+        ></i
+      >
+      <i
+        v-if="$store.state.token != ''"
+        class="fa-solid fa-arrow-right-to-bracket"
+        ><router-link class="link-text" v-bind:to="{ name: 'logout' }"
+          >Logout</router-link
+        ></i
+      >
       <!-- &nbsp;|&nbsp; -->
-      
     </div>
     <div id="page">
       <div class="title">
         <h1>TE-Gram</h1>
+        <div class="dropdown-search-menu">
+          <input
+            v-model="searchQuery"
+            type="text"
+            placeholder="Search for User"
+          />
+          <div class="options">
+            <ul>
+              <li>List Item</li>
+              <li>List Item</li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <router-view/>
+      <router-view />
     </div>
   </div>
 </template>
@@ -49,17 +74,43 @@
 
 #page {
   grid-area: link;
- 
+
   width: 100%;
-  
 }
 
 .title {
-  background-color: rgba(255, 51, 102, 0.20);
+  background-color: rgba(255, 51, 102, 0.2);
   height: 75px;
   width: 100%;
 
   text-align: center;
 }
 
+.dropdown-search-menu {
+  max-width: 350px;
+  position: relative;
+  margin: 0 auto;
+}
+
+.selected-item {
+  height: 40px;
+  border: 2px solid lightgray;
+  border-radius: 5px;
+  padding: 5px 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 16px;
+  font-weight: 500;
+}
+
+.dropdown-popover {
+  position: absolute;
+  border: 2px solid lightgray;
+  top: 46px;
+  left: 0;
+  right: 0;
+  background-color: #fff;
+  max-width: 100%;
+}
 </style>
