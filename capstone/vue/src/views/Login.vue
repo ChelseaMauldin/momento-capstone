65972d8282ac19b37b1c69361647a9a69e4121a2
Vanessa <vmaimone@gmail.com>
2023-02-08 14:54:00 -0500

    gradient to login

--- capstone/vue/src/views/Login.vue
+++ capstone/vue/src/views/Login.vue
@@ -138,9 +138,7 @@ export default {
   flex-direction: row;
   justify-content:center;
   /*height: 100vh;*/
-  font-family: Arial, Helvetica, sans-serif;
-  background: rgb(238,174,202);
-  background: linear-gradient(90deg, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);
+  
 
 }
 #image-login {
