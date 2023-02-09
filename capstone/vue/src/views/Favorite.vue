acbbea45bf38fbca8e26a24590654bbf8d0e5818
Chelsea Mauldin <clmauldin5962@gmail.com>
2023-02-09 12:47:33 -0500

    fixed shadow around logo

--- capstone/vue/src/views/Favorite.vue
+++ capstone/vue/src/views/Favorite.vue
@@ -35,18 +35,23 @@ h3.favorite-title {
   text-align: center;
   font-size: 3rem;
   font-weight: bolder;
-  color: rgb(188,141,221);
-color: linear-gradient(-90deg, rgba(188,141,221,1) 25%, rgba(138,193,255,1) 50%, rgba(241,134,134,1) 100%);
-margin: 10px;
+  color: rgb(188, 141, 221);
+  color: linear-gradient(
+    -90deg,
+    rgba(188, 141, 221, 1) 25%,
+    rgba(138, 193, 255, 1) 50%,
+    rgba(241, 134, 134, 1) 100%
+  );
+  margin: 10px;
 }
 div.main-favorites {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 div.favorite-posts-container {
-    align-self: center;
+  align-self: center;
   display: flex;
   flex-basis: 100%;
   flex-wrap: wrap;
@@ -54,7 +59,8 @@ div.favorite-posts-container {
 }
 
 .favorite-post {
-    width: 25%;
-    margin: 10px;
+  width: 25%;
+  margin: 10px;
+  height: fit-content;
 }
 </style>
